536036ab503eb266e94b0320ef053e623f6e07f0
RyanBcktt <Ryan.beckett@outlook.com>
2017-12-17 00:36:49 +0000

    0.7.6

--- logoAnimation.js
+++ logoAnimation.js
@@ -8,7 +8,7 @@ window.onload = function() {
     var logo2 = document.getElementById("logo2");
   
     //set frequency(FPS) of animation
-    var time = setInterval(expand, 10);
+    var time = setInterval(expand, 1);
   
     //function that sets animation speed and dur
     function expand() {
@@ -16,7 +16,7 @@ window.onload = function() {
         clearInterval(time);
       }
       else {
-        ltv += 0.05;
+        ltv += 0.02;
         logo1.style.letterSpacing = ltv+"px";
         logo2.style.letterSpacing = ltv+"px";
       }
