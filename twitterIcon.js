528b4744a3e5361ffad0f67fb8f7d8c22bd911cb
RyanBcktt <Ryan.beckett@outlook.com>
2017-12-21 23:18:53 +0000

    0.8

---
+++ twitterIcon.js
@@ -0,0 +1,7 @@
+  function hover(element) {
+    element.setAttribute('src', 'Twitter_Hover_Icon.png');
+  }
+
+  function unhover(element) {
+    element.setAttribute('src', 'twitter_Default_Icon.png');
+  }
