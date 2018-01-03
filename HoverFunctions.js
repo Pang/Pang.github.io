fd87082c106e480047646f768d0efe78a6affcee
RyanBcktt <Ryan.beckett@outlook.com>
2018-01-03 21:57:06 +0000

    0.9

---
+++ HoverFunctions.js
@@ -0,0 +1,15 @@
+function hoverTwit(element) {
+  element.setAttribute('src', 'Twitter_Hover_Icon.png');
+}
+
+function unhoverTwit(element) {
+  element.setAttribute('src', 'Twitter_Default_Icon.png');
+}
+
+function hoverLinkd(element) {
+  element.setAttribute('src', 'Linkd_Hover_Icon.png');
+}
+
+function unhoverLinkd(element) {
+  element.setAttribute('src', 'Linkd_Default_Icon.png');
+}
--- twitterIcon.js
+++
@@ -1,7 +0,0 @@
-  function hover(element) {
-    element.setAttribute('src', 'Twitter_Hover_Icon.png');
-  }
-
-  function unhover(element) {
-    element.setAttribute('src', 'Twitter_Default_Icon.png');
-  }
