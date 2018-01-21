aca41ed9fb2a543436f18cfb79f436d4d2d60a4c
RyanBcktt <Ryan.beckett@outlook.com>
2018-01-21 21:06:37 +0000

    1.7

--- logoAnimation.js
+++ logoAnimation.js
@@ -1,39 +1,42 @@
-window.onload = function() {
+window.addEventListener("load", function startPageEvent() {
     
-    //current letter spacing value
-    var lsv = 0;
-  
-    //logo ID element
-    var logo1 = document.getElementById("logo1");
-    var logo2 = document.getElementById("logo2");
-  
-    //set interval of expand function by 1 millisecond
-    var time = setInterval(expand, 1);
-  
-    //function that sets animation speed and dur
-    function expand() {
-      if (lsv >= 15) {
-        clearInterval(time);
-      }
-      else if (lsv < 10) {
-        lsv += 0.05;
-        logo1.style.letterSpacing = lsv+"px";
-        logo2.style.letterSpacing = lsv+"px";
-      } 
-      else if (lsv < 12) { 
-        lsv += 0.03;
-        logo1.style.letterSpacing = lsv+"px";
-        logo2.style.letterSpacing = lsv+"px";
-      } 
-      else if (lsv < 14) { 
-        lsv += 0.02;
-        logo1.style.letterSpacing = lsv+"px";
-        logo2.style.letterSpacing = lsv+"px";
-      } 
-      else {
-        lsv += 0.01;
-        logo1.style.letterSpacing = lsv+"px";
-        logo2.style.letterSpacing = lsv+"px";
-      }
+  //current letter spacing value.
+  let lsv = 0;
+
+  //logo ID element
+  const logo1 = document.getElementById("logo1");
+  const logo2 = document.getElementById("logo2");
+
+  //set interval of expand function by 10 milliseconds.
+  const time = setInterval(expand, 10);
+
+  //changes the letterSpacing to equal the 'lsv' value.
+  const logoLs = x => { x.style.letterSpacing = lsv+"px";}
+
+  //function that sets animation speed and dur.
+  function expand() {
+    if (lsv >= 15) {
+      clearInterval(time);
+    }
+    else if (lsv < 10) {
+      lsv += 0.16;
+      logoLs(logo1);
+      logoLs(logo2);
+    } 
+    else if (lsv < 12) { 
+      lsv += 0.09;
+      logoLs(logo1);
+      logoLs(logo2);
+    } 
+    else if (lsv < 14) { 
+      lsv += 0.06;
+      logoLs(logo1);
+      logoLs(logo2);
+    } 
+    else {
+      lsv += 0.03;
+      logoLs(logo1);
+      logoLs(logo2);
     }
   }
+});
