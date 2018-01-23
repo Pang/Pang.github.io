4b55423737ab45552e7c98644496ea562ab8b93b
RyanBcktt <Ryan.beckett@outlook.com>
2018-01-23 23:02:42 +0000

    1.7.5

--- navBarJs.js
+++ navBarJs.js
@@ -1,18 +1,47 @@
-window.addEventListener("load", function Nav() { 
+window.addEventListener("load", () => { 
 
-const navigate = `
-    <ul>
-        <li class="MainHeader">&nbsp;RYAN BECKETT</li>
-        <li><a id="menuBtn" href="index.html">Home</a></li>
-        <li><a id="menuBtn" href="portfolioAboutMe.html">About Me</a></li>
-        <li><a id="menuBtn" href="portfolioProjects.html">Projects</a></li>
-        <li><a id="menuBtn" href="portfolioContact.html">Contact</a></li>
-        <li><a href="https://www.Twitter.com/RyanBcktt" target="_blank"><img id="twitterLogo" src="Twitter_Default_Icon.png" onmouseover="hoverTwit(this)" onmouseout="unhoverTwit(this)"/></a></li>
-        <li><a href="https://www.linkedin.com/in/ryan-beckett-75112510b/" target="_blank"><img id="LinkedInLogo" src="Linkd_Default_Icon.png" onmouseover="hoverLinkd(this)" onmouseout="unhoverLinkd(this)"/></a></li>
-        <li><a href="https://gist.github.com/cPango/" target="_blank"><img id="GitHubLogo" src="Git_Default_Icon.png" onmouseover="hoverGit(this)" onmouseout="unhoverGit(this)"/></a></li>
-    </ul>
-`;
+    const navigate = `
+        <ul>
+            <li class="MainHeader">&nbsp;RYAN BECKETT</li>
+            <li><a id="menuBtn" href="index.html">Home</a></li>
+            <li><a id="menuBtn" href="portfolioAboutMe.html">About Me</a></li>
+            <li><a id="menuBtn" href="portfolioProjects.html">Projects</a></li>
+            <li><a id="menuBtn" href="portfolioContact.html">Contact</a></li>
+            <li><a href="https://www.Twitter.com/RyanBcktt" target="_blank"><img class="logoChange" id="twitterLogo" src="Twitter_Default_Icon.png" /></a></li>
+            <li><a href="https://www.linkedin.com/in/ryan-beckett-75112510b/" target="_blank"><img class="logoChange" id="LinkedInLogo" src="Linkd_Default_Icon.png"/></a></li>
+            <li><a href="https://gist.github.com/cPango/" target="_blank"><img class="logoChange" id="GitHubLogo" src="Git_Default_Icon.png"/></a></li>
+        </ul>
+    `;
 
-document.querySelector('.navigateBar').innerHTML = navigate;
+    document.querySelector('.navigateBar').innerHTML = navigate;
 
-});
+
+    const logoChange = document.querySelectorAll(".logoChange");
+
+    for ( let i = 0 ; i < logoChange.length ; i++ ) {
+        logoChange[i].addEventListener("mouseover", () => {
+            if(logoChange[i].id == "twitterLogo"){
+                logoChange[i].src = 'Twitter_Hover_Icon.png';
+            } 
+            if (logoChange[i].id == "LinkedInLogo") {
+                logoChange[i].src = 'Linkd_Hover_Icon.png';
+            } 
+            if(logoChange[i].id == "GitHubLogo"){
+                logoChange[i].src = 'Git_Hover_Icon.png';
+            }
+        });
+
+        logoChange[i].addEventListener("mouseout", () => {
+            if(logoChange[i].id== "twitterLogo"){
+                logoChange[i].src = 'Twitter_Default_Icon.png';
+            } 
+            if (logoChange[i].id == "LinkedInLogo") {
+                logoChange[i].src = 'Linkd_Default_Icon.png';
+            } 
+            if(logoChange[i].id == "GitHubLogo"){
+                logoChange[i].src = 'Git_Default_Icon.png';
+            }
+        });
+    }
+
+})
