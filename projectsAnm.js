76a90472e15cb72995f4028ff9e81eb155f285ed
RyanBcktt <Ryan.beckett@outlook.com>
2018-02-09 01:02:20 +0000

    2.1.7

--- projectsAnm.js
+++ projectsAnm.js
@@ -5,18 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
     $('ol').eq(2).slideUp(); 
 
     //KnightVsFoe
-    $('div').eq(2).hover(function() {
+    $('div').eq(1).hover(function() {
         $('ol').eq(0).slideDown();
     });
-    $('div').eq(2).mouseleave(function() {
+    $('div').eq(1).mouseleave(function() {
         $('ol').eq(0).slideUp();
     });
 
     //ListCreator
-    $('div').eq(4).hover(function() {
+    $('div').eq(2).hover(function() {
         $('ol').eq(1).slideDown();
     });
-    $('div').eq(4).mouseleave(function() {
+    $('div').eq(2).mouseleave(function() {
         $('ol').eq(1).slideUp();
     });
 });
