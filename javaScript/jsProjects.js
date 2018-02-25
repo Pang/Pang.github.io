37b920c36d8eb7d101959a31609a8fc416ae990e
cPango <ryan.beckett@outlook.com>
2018-02-25 22:44:15 +0000

    2.3.8.6

--- javaScript/jsProjects.js
+++ javaScript/jsProjects.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     $('ol').eq(1).slideUp();
     $('ol').eq(2).slideUp();
     $('ol').eq(3).slideUp();  
+    $('ol').eq(4).slideUp(); 
 
     //AdminNotes
     $('div').eq(4).hover(function() {
@@ -26,5 +27,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     $('div').eq(6).mouseleave(function() {
         $('ol').eq(3).slideUp();
+    });
+
+    //TicTacBoom
+    $('div').eq(7).hover(function() {
+        $('ol').eq(4).slideDown();
+    });
+    $('div').eq(7).mouseleave(function() {
+        $('ol').eq(4).slideUp();
     });
 });
