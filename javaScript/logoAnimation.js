0b0b8678ac03249333af37b35686595ccb456ce7
cPango <ryan.beckett@outlook.com>
2018-02-26 23:17:40 +0000

    [D2.4

--- javaScript/logoAnimation.js
+++ javaScript/logoAnimation.js
@@ -7,36 +7,45 @@ window.addEventListener("load", function startPageEvent() {
   const logo1 = document.getElementById("logo1");
   const logo2 = document.getElementById("logo2");
 
-  //set interval of expand function by 10 milliseconds.
-  const time = setInterval(expand, 20);
-
   //changes the letterSpacing to equal the 'lsv' value.
   const logoLs = x => { x.style.letterSpacing = lsv+"px";}
 
-  //function that sets animation speed and dur.
-  function expand() {
-    if (lsv >= 30) {
-      clearInterval(time);
+  if ( $(window).width() > 539) {      
+
+    //set interval of expand function by 10 milliseconds.
+    const time = setInterval(expand, 10);
+
+    //function that sets animation speed and dur.
+    function expand() {
+      if (lsv >= 31) {
+        clearInterval(time);
+      }
+      else if (lsv < 20) {
+        lsv += 0.22;
+        logoLs(logo1);
+        logoLs(logo2);
+      } 
+      else if (lsv < 27) { 
+        lsv += 0.14;
+        logoLs(logo1);
+        logoLs(logo2);
+      } 
+      else if (lsv < 29) { 
+        lsv += 0.06;
+        logoLs(logo1);
+        logoLs(logo2);
+      } 
+      else {
+        lsv += 0.03;
+        logoLs(logo1);
+        logoLs(logo2);
+      }
     }
-    else if (lsv < 27) {
-      lsv += 0.18;
-      logoLs(logo1);
-      logoLs(logo2);
-    } 
-    else if (lsv < 28) { 
-      lsv += 0.09;
-      logoLs(logo1);
-      logoLs(logo2);
-    } 
-    else if (lsv < 29) { 
-      lsv += 0.06;
-      logoLs(logo1);
-      logoLs(logo2);
-    } 
-    else {
-      lsv += 0.02;
-      logoLs(logo1);
-      logoLs(logo2);
-    }
+  } 
+  else 
+  {
+    lsv = 10;
+    logoLs(logo1);
+    logoLs(logo2);
   }
 });
