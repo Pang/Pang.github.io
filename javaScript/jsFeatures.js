3b5f037b4c1cba502598a3c65eab0f7cc851d698
cPango <Ryan.beckett@outlook.com>
2018-03-09 19:43:07 +0000

    Desktop-view layout changes

--- javaScript/jsFeatures.js
+++ javaScript/jsFeatures.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelector("#links");
 
     links.addEventListener('mouseover', (x) => {
-        if ( x.target.id == "twitterLogo" ) { x.target.src = 'icons/Twitter_Hover_Icon.png'; } 
-        else if ( x.target.id == "LinkedInLogo" ) { x.target.src = "icons/Linkd_Hover_Icon.png"; }
-        else if ( x.target.id == "GitHubLogo" ) { x.target.src = "icons/Git_Hover_Icon.png"; }
+        if ( x.target.id === "twitterLogo" ) { x.target.src = 'icons/Twitter_Hover_Icon.png'; } 
+        else if ( x.target.id === "LinkedInLogo" ) { x.target.src = "icons/Linkd_Hover_Icon.png"; }
+        else if ( x.target.id === "GitHubLogo" ) { x.target.src = "icons/Git_Hover_Icon.png"; }
     })
     links.addEventListener('mouseout', (x) => {
-        if ( x.target.id == "twitterLogo" ) { x.target.src = 'icons/Twitter_Default_Icon.png'; } 
-        else if ( x.target.id == "LinkedInLogo" ) { x.target.src = "icons/Linkd_Default_Icon.png"; }
-        else if ( x.target.id == "GitHubLogo" ) { x.target.src = "icons/Git_Default_Icon.png"; }
+        if ( x.target.id === "twitterLogo" ) { x.target.src = 'icons/Twitter_Default_Icon.png'; } 
+        else if ( x.target.id === "LinkedInLogo" ) { x.target.src = "icons/Linkd_Default_Icon.png"; }
+        else if ( x.target.id === "GitHubLogo" ) { x.target.src = "icons/Git_Default_Icon.png"; }
     })
 
     //Skip through projects feature
