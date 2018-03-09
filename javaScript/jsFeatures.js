23b90725bcc9b492e4640ff689faf1a3209fa079
cPango <Ryan.beckett@outlook.com>
2018-03-09 19:06:14 +0000

    Changed display of projects

--- javaScript/jsFeatures.js
+++ javaScript/jsFeatures.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => { 
 
+    //Hover over Icons feature
     const links = document.querySelector("#links");
 
     links.addEventListener('mouseover', (x) => {
@@ -12,6 +13,33 @@ document.addEventListener("DOMContentLoaded", () => {
         else if ( x.target.id == "LinkedInLogo" ) { x.target.src = "icons/Linkd_Default_Icon.png"; }
         else if ( x.target.id == "GitHubLogo" ) { x.target.src = "icons/Git_Default_Icon.png"; }
     })
+
+    //Skip through projects feature
+    let slideIndex = 1;
+    showProjects(slideIndex);
+
+    $("#rightArrow").click(function() { nextProj(1) } );
+    $("#leftArrow").click(function() { nextProj(-1) } );
+
+    function nextProj(n) {
+        showProjects(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showProjects(slideIndex = n);
+    }
+
+    function showProjects(n) {
+        let myProjects = document.querySelectorAll(".codeProjects");
+        
+        if (n > myProjects.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = myProjects.length }
+
+        for (let i = 0; i < myProjects.length; i++) {
+            myProjects[i].style.display = "none";
+        }
+        myProjects[slideIndex - 1].style.display = "block";
+    }
 
 });
 
