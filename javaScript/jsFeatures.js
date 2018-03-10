f5d49af7a7cc6c82220d2939c713f348f9a8f5d0
cPango <Ryan.beckett@outlook.com>
2018-03-10 19:46:38 +0000

    css changes and removed unneccessary Javascript

--- javaScript/jsFeatures.js
+++ javaScript/jsFeatures.js
@@ -1,23 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => { 
 
-    //Hover over Icons feature
-    const links = document.querySelector("#links");
+    //current letter spacing value.
+    let lsv = 0;
 
-    links.addEventListener('mouseover', (x) => {
-        if ( x.target.id === "twitterLogo" ) { x.target.src = 'icons/Twitter_Hover_Icon.png'; } 
-        else if ( x.target.id === "LinkedInLogo" ) { x.target.src = "icons/Linkd_Hover_Icon.png"; }
-        else if ( x.target.id === "GitHubLogo" ) { x.target.src = "icons/Git_Hover_Icon.png"; }
-    })
-    links.addEventListener('mouseout', (x) => {
-        if ( x.target.id === "twitterLogo" ) { x.target.src = 'icons/Twitter_Default_Icon.png'; } 
-        else if ( x.target.id === "LinkedInLogo" ) { x.target.src = "icons/Linkd_Default_Icon.png"; }
-        else if ( x.target.id === "GitHubLogo" ) { x.target.src = "icons/Git_Default_Icon.png"; }
-    })
+    //logo ID element
+    const logo1 = document.getElementById("logo1");
+    const logo2 = document.getElementById("logo2");
 
-    //Skip through projects feature
+    //changes the letterSpacing to equal the 'lsv' value.
+    const logoLs = x => { x.style.letterSpacing = lsv + "px"; }
+
+    if ($(window).width() > 539) {
+
+        //set interval of expand function by 10 milliseconds.
+        const time = setInterval(expand, 10);
+
+        //function that sets animation speed and dur.
+        function expand() {
+            if (lsv >= 31) {
+                clearInterval(time);
+            }
+            else if (lsv < 20) {
+                lsv += 0.22;
+                logoLs(logo1);
+                logoLs(logo2);
+            }
+            else if (lsv < 27) {
+                lsv += 0.14;
+                logoLs(logo1);
+                logoLs(logo2);
+            }
+            else if (lsv < 29) {
+                lsv += 0.06;
+                logoLs(logo1);
+                logoLs(logo2);
+            }
+            else {
+                lsv += 0.03;
+                logoLs(logo1);
+                logoLs(logo2);
+            }
+        }
+    }
+    else {
+        lsv = 10;
+        logoLs(logo1);
+        logoLs(logo2);
+    }
     let slideIndex = 1;
     showProjects(slideIndex);
 
+    //Slideshow for Projects
     $("#rightArrow").click(function() { nextProj(1) } );
     $("#leftArrow").click(function() { nextProj(-1) } );
 
@@ -40,6 +73,5 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         myProjects[slideIndex - 1].style.display = "block";
     }
-
 });
 
