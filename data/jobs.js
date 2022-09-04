dd582618237774ea49291de25a8f8b0be5a5123f
cPango <Ryan.beckett@outlook.com>
2022-09-05 00:00:00 +0100

    job data started

---
+++ data/jobs.js
@@ -0,0 +1,21 @@
+const jobs = [
+    {
+        "id" : 1,
+        "employer" : "WebContractor",
+        "datesWorked" : "May 2022 – Present",
+        "description" : 
+            "<p>WebContractor utilizes C# for full-stack development, with Blazor for creating front-end interfaces and .Net for building microservices, I have been able to be a part of all areas of development in their core product. Daily scrums and Azure Dev-Ops allow us to make sure we are working on the right tasks and on track for when it comes to sharing our progress with clients when requests to functionsality have been made.</p><p>WebContractor's main product is for construction Contractors who need a solution to simplifying and improving the efficiencey of managing multiple sub-contractors on a project. Companies can manage applications for payments and invoices more efficiently, whilst ensuring compliance with the UK Construction Act.</p>"
+    },
+    {
+        "id" : 2,
+        "employer" : "Pastest",
+        "datesWorked" : "",
+        "description" : ""
+    },
+    {
+        "id" : 3,
+        "employer" : "Blackpool Victoria Hospital",
+        "datesWorked" : "",
+        "description" : ""
+    },
+];
