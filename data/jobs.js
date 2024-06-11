504cb3a1c57f2c1ceeda5f6df454c8671450cb9a
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2024-06-11 21:23:00 +0100

    Update jobs.js

--- data/jobs.js
+++ data/jobs.js
@@ -3,13 +3,13 @@ const jobs = [
         "id" : 1,
         "employer" : "Holovis",
         "datesWorked" : "December 2023 – Present",
-        "description" : "<p>Working as a Software Solutions Architect I get to contribute to many different areas of a project. From planning and structuring the architecture of code, creating the application in Unity then building microservices or API’s that are needed alongside it. I also get involved with the embedded programming side of things when needed.</p> <p>A lot of my time is also spent creating Ansible scripts to deploy all our projects across multiple machines or building test pipelines to build new releases of code for the number of attractions we design and build for our clients.</p>"
+        "description" : "<p>Working as a Software Solutions Architect I get to contribute to many different areas of a project. From planning and structuring the architecture of code, creating the application in Unity then building microservices or API’s that are needed alongside it. I also get involved with the embedded programming side of things when needed.</p> <p>A lot of my time is also spent creating Ansible scripts to deploy all our projects across multiple machines or building Jenkins test pipelines to build new releases of code for the number of attractions we design and build for our clients.</p>"
     },
     {
         "id" : 2,
         "employer" : "WebContractor",
         "datesWorked" : "May 2022 – November 2023",
-        "description" : "<p>WebContractor utilizes C# for full-stack development, with Blazor for creating front-end interfaces and .Net for building microservices. I have been able to be a part of all areas of development in their core product. Daily scrums and Azure Dev-Ops allow us to make sure we are working on the right tasks and on track for when it comes to sharing our progress with clients when change-requests are made to functionality.</p><p>WebContractor's main product is for contractors who need a solution to simplify and improve the efficiency of managing multiple subcontractors on a single or multiple projects. Companies can also manage applications for payments and invoices from the app whilst ensuring compliance with the UK Construction Act.</p>"
+        "description" : "<p>WebContractor utilizes C# for full-stack development, with Blazor for creating front-end interfaces and .Net for building microservices. I have been able to be a part of all areas of development in their core product. Daily scrums and Azure Dev-Ops allow us to easily prioritise the right features for our clients and follow correct CI/CD processes by creating pipelines for building and releasing to test servers.</p>"
     },
     {
         "id" : 3,
