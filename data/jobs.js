4ed8a0614301505daff99c1c6d429c18a8b2fe29
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2024-06-15 17:52:15 +0100

    Update jobs.js

--- data/jobs.js
+++ data/jobs.js
@@ -9,18 +9,18 @@ const jobs = [
         "id" : 2,
         "employer" : "WebContractor",
         "datesWorked" : "May 2022 – November 2023",
-        "description" : "<p>WebContractor utilizes C# for full-stack development, with Blazor for creating front-end interfaces and .Net for building microservices. I have been able to be a part of all areas of development in their core product. Daily scrums and Azure Dev-Ops allow us to easily prioritise the right features for our clients and follow correct CI/CD processes by creating pipelines for building and releasing to test servers.</p>"
+        "description" : "<p>WebContractor utilises C# for full-stack development, with Blazor creating front-end interfaces and .Net for building microservices, I was able to be a part of all areas of development in their core product. Daily scrums and Azure Dev-Ops allow us to easily prioritise the right features for our clients and follow correct CI/CD processes by creating pipelines for building and releasing to test servers.</p>"
     },
     {
         "id" : 3,
         "employer" : "Pastest",
         "datesWorked" : "Oct 2021 – Apr 2022",
-        "description" : "<p>I was hired to build a new 'Microlearning' feature using ASP.Net, which was an extension of their Medical Exam question bank subscription. I was given designs and a functional specification and had weekly meetings to share my progress with the team and managers involved in the project.</p> <p>Whilst here, I also supported the junior developers working on their in-house content management system built in ReactJS. This was used by the content team to create and manage exam questions, learning resources and other materials that would appear in both their web and mobile app.</p>"
+        "description" : "<p>At Pastest, my main role was on the front-end. Features are planned as a team, then my role was to turn those specifications into a complete interface and help build the API/Models alongside it. One of my main priorities was to rebuild the web-app in MVC and develop additional features. I also helped to maintain the in-house content management system, which is used daily by the content team in order to create and edit subscription questions and learning resources, and finally picking up support tickets on the help-desk which contains issues raised from both customers and colleagues.</p>"
     },
     {
         "id" : 4,
         "employer" : "Blackpool Victoria Hospital",
         "datesWorked" : "Aug 2019 – Sep 2021",
-        "description" : "<p>Working with technologies like .Net and Angular, I had been a part of a great collaborative team creating applications to aid the work of the medical and clinical staff at Blackpool Victoria hospital.</p><p>My main focus was on an in-house Electronic Patient Record known as Nexus, which aggregated together many third party systems as well as having it's own functionality for admitting a patient. It followed their journey of care and maintained an overview of each ward in the hospital - allowing for more reliable information retrieval, quicker nurse and doctor handovers and efficient placement of patients to wards which were more suited to their needs.</p>"
+        "description" : "<p>Working with technologies like .Net and Angular, I was part of a great team creating web applications which aided the work of the medical and clinical staff at Blackpool Victoria hospital. My main focus was on an in-house Electronic Patient Record known as Nexus, which aggregated together many third party systems as well as having its own functionality for admitting a patient, following their journey of care and maintaining an overview of each ward in the hospital - allowing for faster, more reliable information retrieval, quicker nurse and doctor handovers as well as more efficient patient flow, allowing for better placement of patients to wards more suited to their needs.</p>"
     },
 ];
