d152f53e66882675923b2fa9ff48dfea81b95002
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2024-12-18 14:49:58 +0000

    removed projects

--- data/projects.js
+++ data/projects.js
@@ -37,37 +37,13 @@ const projects = [
     {
         "id" : 4,
         "type" : "game",
-        "title" : "Smart Golf Tech",
-        "description" : "<p>Whilst at Holovis I worked on multiple smart golf courses. The balls themselves were assigned to players and tracked when they were hit, between being placed on a tee and going into the hole. A couple holes I was heavily invested in were 'Spin & Win' which stopped the wheel for a prize when you hit the ball and 'True or False' which asked the player a question and let you decide which route to go, scoring higher if you're right!</p>",
-        "images" : [
-            "./images/SmartGolf/techGolf1.jpg",
-            "./images/SmartGolf/techGolf2.jpg",
-            "./images/SmartGolf/techGolf3.jpg"
-        ],
-        "video" : ""
-    },
-    {
-        "id" : 5,
-        "type" : "game",
-        "title" : "Squid Games Experience",
-        "description" : "<p>An immersive experience where you play as a Squid Games contestant! My first priority on this project was to build a service that held all the logic for groups playing Memory Steps, splitting them into teams, showing them a pattern and then making them recall that with each step they took. My second priority was to create a warships game where players sit in ships and from there use a control panel and the large screen between them and their opponents to talk tactics and decide their next shot.</p>",
-        "images" : [
-            "./images/SquidyG/squidyG1.jpg",
-            "./images/SquidyG/squidyG2.jpg",
-            "./images/SquidyG/squidyG3.jpg"
-        ],
-        "video" : ""
-    },
-    {
-        "id" : 6,
-        "type" : "game",
         "title" : "Procedural Terrain",
         "description" : "<p>Through the use of perlin noise, creating my own mesh and building a shader for the colours of the terrain depending on it\’s height, I was able to generate different maps and then use “fall-off” to turn them into islands. I shared my findings and how I built this project on a <a class=\"dialoglink\" href=\"https://www.youtube.com/watch?v=cLs3CGNV120\" target=\"blank\">YouTube tutorial</a></p>",
         "images" : [],
         "video" : "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/wmaskscrRvM\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        "id" : 7,
+        "id" : 5,
         "type" : "game",
         "title" : "ShieldMaidens",
         "description" : "<p>ShieldMaidens is a solo/co-op endless runner where you have to fight and block your way through enemies in a randomly generated world so you never go on the same run twice, shared-screen co-op and joypad support. Hand-drawn assets by myself and a combat system that allows the player/s to decide their own style of play. </p><p>You can get a link to games store-page <a class=\"dialoglink\" href=\"https://pangdev.itch.io/shieldmaidens\" target=\"blank\">Here</a></p>",
@@ -75,7 +51,7 @@ const projects = [
         "video" : "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/BKQ04dLX_6w\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        "id" : 8,
+        "id" : 6,
         "type" : "game",
         "title" : "Others",
         "description" : "<p>NPC AI - Inspired by 3D stealth games, uses dot product and lets you crouch to sneak past enemies. <a class=\"dialoglink\" href=\"https://github.com/Pang/NpcLogic\">Code</a></p><p>HexWorld - A top down tower defence like game. Programmatically rendered maps by reading a CSV. NPC's use A* Pathfinding and Players use ‘Breadth-First-Search’ algorithm to find viable paths. <a class=\"dialoglink\" href=\"https://pangdev.itch.io/hexworld\" target=\"blank\">Store page</a></p>",
