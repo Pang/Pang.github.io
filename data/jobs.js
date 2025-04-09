a2a097050cf405dd035b98cd0e5cec7ad0f6e763
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2025-04-09 10:06:35 +0100

    Update jobs.js

--- data/jobs.js
+++ data/jobs.js
@@ -1,30 +1,36 @@
 const jobs = [
     {
         "id" : 1,
+        "employer" : "Eclipse",
+        "datesWorked" : "November 2024 – Present",
+        "description" : "<p>Currently leading on a Blazor .Net project that replicates all the features from their current desktop product, with the aim of building stronger foundations on current technology that can be scaled and maintained easier.</p> <p>Other than building this application as an almost 'greenfield' project, I have set up GitLab, created docker containers for ease of deployment, implemented CI/CD pipelines, written unit and functional tests as well as put together documentation that helps junior developers get straight into contributing on the project.</p>"
+    },
+    {
+        "id" : 2,
         "employer" : "Holovis",
         "datesWorked" : "December 2023 – November 2024",
         "description" : "<p>This role required that I was able to take initiative and contribute to many different technical areas of a project. From planning features, giving time estimates and database architecture whilst considering long term maintainability and scalability through best coding practices. Building interfaces in React or Unity and backend API’s or microservices using .Net Core, depending on the requirements from clients and the use of technical discussions on how to best approach a software solution.</p> <p>TDD was a big part of the development process, NUnit being the preferred .Net testing framework. I would write my own tests first and make sure all of the logic passes, then create a YAML script and set up a multibranch pipeline to create release builds the QA team can use for testing. To get these running on the hardware we build and set up for our clients I would also use a tool called Ansible, which was used to write scripts for setting up and deploying to multiple machines.</p>"
     },
     {
-        "id" : 2,
+        "id" : 3,
         "employer" : "WebContractor",
         "datesWorked" : "May 2022 – November 2023",
         "description" : "<p>Utilising C# for full-stack development, Blazor on the front-end interfaces and .Net for the API, I joined in the early stages of the startup which was able to become a fierce competitor on the market and sold within a couple of years. Azure Dev-Ops was our main tool for fleshing out milestones, sprints and prioritising the right features for our clients. I also used azure pipelines to create CI/CD processes and rollout tests for our QA before releasing.</p><p>I was responsible for many major features which I led technical discussions for with my team. Alongside creating milestones and breaking these down into user stories and tasks then giving time estimations for. I would have regular meetings to keep project managers up to date on progress and when clients wanted a change request I was always motivated to stay flexible and find a way to deliver what was needed without pushing back deadlines.</p>"
     },
     {
-        "id" : 3,
+        "id" : 4,
         "employer" : "Pastest",
         "datesWorked" : "Oct 2021 – Apr 2022",
         "description" : "<p>My main objective at Pastest was to build a Microlearning web-app that sat beside their main core product as another tool for doctors and medical professionals to use and practise for their exams. I helped plan the features with the in-house team and then turned the specifications we came up with into a complete application, from creating the UI, the tables in the database, the API and all the shared DTO models.</p>"
     },
     {
-        "id" : 4,
+        "id" : 5,
         "employer" : "Blackpool Victoria Hospital",
         "datesWorked" : "Aug 2019 – Sep 2021",
         "description" : "<p>Working with technologies like .Net and Angular, I was a large contributor to their in-house EPR system Nexus, which aided the work of the medical and clinical staff.This app was built to aggregate many third party systems as well as having its own functionality for admitting a patient, following their journey of care and maintaining an overview of each ward in the hospital.</p>"
     },
     {
-        "id" : 5,
+        "id" : 6,
         "employer" : "Freelance",
         "datesWorked" : "Aug 2017 – Sep 2019",
         "description" : "<p>Whilst working full time, I taught myself to code websites with html, CSS and javascript, then used these skills to build web pages for small businesses. These were statically hosted and I handled the domains myself so clients didn’t have to worry about additional admin.</p>"
